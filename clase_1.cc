3e904dda1372e81fff893f7311d0029fd087008c
Rubio00 <00052524@uca.edu.sv>
2025-08-20 12:29:07 -0600

    :sparkles:feat: agregando funciones para registro

--- clase_1.cc
+++ clase_1.cc
@@ -1,39 +1,70 @@
 #include <iostream>
 #include <ctime>
 
+std::string GenerarCupon(std::string Letras);
+void PremioCupon(std::string premio);
 
-std:: string GenerarCupon (std::string Letras );
-void PremioCupon (std:: string premio);
-
-int main(){
-
+int main()
+{
     srand(time(0));
-    std:: string cupon_generado =  GenerarCupon("AGO");
+    std::string cupon_generado = GenerarCupon("AGO");
     PremioCupon(cupon_generado);
 
-   return 0; 
+    int cantidad;
+    std:: cout << "Ingrese la cantidad de cupones a generar ";
+    std:: cin >> cantidad;
+
+    std:: string cupones[cantidad];
+
+    return 0;
 }
 
-std:: string GenerarCupon (std::string Letras ){
-    //generar el valor aleatorio 
+std::string GenerarCupon(std::string Letras)
+{
+    // generar el valor aleatorio
     int numero_aleatorio = rand() % 100 + 999;
-    std:: string cadena = std:: to_string(numero_aleatorio);
+    std::string cadena = std::to_string(numero_aleatorio);
 
-    return Letras + cadena; 
+    return Letras + cadena;
 }
 
-void PremioCupon(std::string premio){
-    //Extraer el valor numerico del identificador
-    premio.substr(3,4);
+void PremioCupon(std::string premio)
+{
+    // Extraer el valor numérico (a partir del 3er carácter en adelante)
+    std::string parte_numerica = premio.substr(3);
 
-    //convertir a entero
-    int conversion_entero = std:: stoi(premio);
+    // convertir a entero
+    int conversion_entero = std::stoi(parte_numerica);
 
-    //Evualuar si es par o no par 
-    if(premio % 2 == 0)
+    // Evaluar si es par o no par
+    if (conversion_entero % 2 == 0)
     {
-        std:: cout << "tienes premio ";
-    }else {
-        std:: cout << "no tienes premio ";
+        std::cout << "tienes premio ";
     }
+    else
+    {
+        std::cout << "no tienes premio ";
+    }
+}
+
+std::string SolicitarDatos()
+{
+    std::string prefijo;
+
+    do
+    {
+        std::cout << "Ingresa el prefijo del cupon : ";
+        std::cin >> prefijo;
+
+        if (prefijo.length() == 3)
+        {
+            return prefijo;
+        }
+        else
+        {
+            return "Recuerda que son 3 letras maximo ";
+        }
+    }while(true);
+    
+    
 }
